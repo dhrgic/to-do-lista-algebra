690689edb47fa7b440a9a732cc67267527e37da6
dhrgic <dhrgic@gmail.com>
2023-02-12 17:55:25 +0100

    Nesto sam napravio

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+window.addEventListener("load", (ucitavanje) => {
+    ucitavanje.preventDefault();
+    const formEl = document.querySelector("#myForm");
+    const taskInput = document.querySelector("#taskInput");
+    const tasksEl = document.querySelector("#tasks");
+    let tasksArray = [];
+    
 
+    formEl.addEventListener("keydown", (e) => {
+        
+        
+        if(event.key === "Enter") {
+            e.preventDefault();
+            let todo = taskInput.value;
+            if (todo === "") {alert("Zadatak prazan. Unesi zadatak"); return}
+
+            const taskEl = document.createElement("div");
+            taskEl.classList.add("todo__task");
+            tasksEl.appendChild(taskEl);
+            tasksArray.push(todo);
+            console.log(tasksArray);
+
+            const taskText = document.createElement("input");
+            taskText.setAttribute("type", "text");
+            taskText.classList.add("todo__text");
+            taskText.value = todo;
+            taskText.setAttribute("readonly", "readonly");
+            taskEl.appendChild(taskText);
+            formEl.reset();
+        }
+    })
+})
