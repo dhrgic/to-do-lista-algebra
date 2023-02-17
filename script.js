d31abba4bb085e18fb79c678855b426afd65e0a4
dhrgic <dhrgic@gmail.com>
2023-02-17 19:07:46 +0100

    Predavanje

---
+++ script.js
@@ -0,0 +1,37 @@
+// const processOrder = function (name, meal, estimation, callback) {
+//   const status = `${name}'s ${meal} is preparing!`;
+//   callback(status);
+
+//   setTimeout(function () {
+//     const status = `${name}'s ${meal} is prepared!`;
+//     callback(status);
+//   }, estimation);
+// };
+
+// console.log("Matilda orders ....");
+// processOrder("Matilda", "Cheesburger", 2000, function (status) {
+//   console.log(status);
+// });
+
+// console.log("Joseph orders ....");
+// processOrder("Joseph", "Salad", 10000, function (status) {
+//   console.log(status);
+// });
+
+// console.log("Tina orders ....");
+// processOrder("Tina", "Filet mignon", 8000, function (status) {
+//   console.log(status);
+// });
+
+// function zbroj(x, y, callback) {
+//   const z = x + y;
+//   callback(z);
+// }
+
+// zbroj(2, 10, function (z) {
+//   if (z < 10) {
+//     console.log(" Zbroj je manji od 10");
+//   } else {
+//     console.log("Zbroj je veći od 10");
+//   }
+// });
